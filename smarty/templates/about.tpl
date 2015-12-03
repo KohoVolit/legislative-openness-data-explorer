ea22e2e52305dc3ff5e01888966b21dda84aa556
Michal Skop <michal.skop@kohovolit.eu>
2015-12-03 04:24:55 +0100

    adding menu into about, enhancement #2

--- smarty/templates/about.tpl
+++ smarty/templates/about.tpl
@@ -1,4 +1,24 @@
 {extends file='main.tpl'}
 {block name=body}
-    {$main_text}
+<div class="row">
+    <div class="col-md-9" role="main">
+        {$main_text}
+    </div>
+    <div id='navbar-example' class="col-md-3" role="complementary">
+        <nav class="bs-docs-sidebar hidden-print hidden-xs hidden-sm affix">
+            <ul class="nav bs-docs-sidenav">
+            {foreach $headers as $h0}
+                <li><a href="#{$h0['slug']}">{$h0["header"]}</a>
+                {if count($h0["children"]) > 0}
+                    <ul class="nav">
+                    {foreach $h0["children"] as $h1}
+                        <li><a href="#{$h1['slug']}">{$h1["header"]}</a>
+                    {/foreach}
+                    </ul>
+                {/if}
+            {/foreach}
+            </ul>
+        </nav>
+    </div>
+</div>
 {/block}
--- www/about/index.php
+++ www/about/index.php
@@ -12,8 +12,87 @@ include('../Parsedown.php');
 $mdurl = TEXT_URL . lang($page) . "/about/about.md";
 $contents = file_get_contents($mdurl);
 $Parsedown = new Parsedown();
-$smarty->assign('main_text',$Parsedown->text($contents));
+$main_text = $Parsedown->text($contents);
+
+
+//prepare headers for menu
+$text_ar = explode("\n",$Parsedown->text($contents));
+$levels = [];
+$hs = [];
+foreach($text_ar as $key=>$row) {
+    for ($level = 2; $level <= 6; $level++) {
+        $m = preg_match("#<h$level>(.*?)</h$level>#",$row,$match);
+        if ($m) {
+            $levels[$level] = $level;
+            $hs[] = [
+                "level" => $level,
+                "header" => $match[1],
+            ];
+            $slug = slugify($match[1]);
+            $text_ar[$key] = "<a name='$slug' id='$slug' class='h$level'>".$match[0]."</h$level></a>";
+        }
+    }
+}
+$smarty->assign('main_text',implode("\n",$text_ar));
+
+asort($levels);
+$h2level = [];
+$i = 0;
+foreach($levels as $key => $l) {
+    $h2level[$key] = $i;
+    $i++;
+}
+$current = 0;
+$headers = [];
+$i = 0;
+foreach($hs as $h) {
+    if ($h2level[$h["level"]] == 0) {
+        $headers[$i] = [
+            "header" => $h["header"],
+            "slug" => slugify($h["header"]),
+            "children" => []
+        ];
+        $i++;
+    }
+    if ($h2level[$h["level"]] == 1) {
+        $headers[$i-1]['children'][] = [
+            "header" => $h["header"],
+            "slug" =>slugify($h["header"])
+        ];
+    }
+}
+
+$smarty->assign('headers',$headers);
 
 $smarty->display($page . '.tpl');
+
+
+
+
+
+function slugify($text)
+{ 
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  if (empty($text))
+  {
+    return 'n-a';
+  }
+
+  return $text;
+}
 
 ?>
