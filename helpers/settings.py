1d71d6ab16f6bcce4fcac6c4585173717bfe3843
Michal Skop <michal.skop@kohovolit.eu>
2016-06-17 04:37:10 +0200

    settings.py example

--- helpers/settings.py
+++
@@ -1,3 +0,0 @@
-#settings for LegislativeExplorerTextBot
-git_dir = "/home/michal/project/legislative-openness-data-explorer-texts"
-ssh_file = "/home/michal/.ssh/LegislativeExplorerTextBot"
---
+++ helpers/settings_example.py
@@ -0,0 +1,7 @@
+#settings for LegislativeExplorerTextBot
+git_dir = "/home/projects/legislative-openness-data-explorer-texts/"
+ssh_file = "/home/user/.ssh/LegislativeExplorerTextBot"
+bot_name = "LegislativeExplorerTextBot"
+bot_email = "LegislativeExplorerTextBot@kohovolit.eu"
+raw_origin = 'https://raw.githubusercontent.com/KohoVolit/legislative-openness-data-explorer-texts/master/'
+csv_url = "https://docs.google.com/spreadsheets/d/google_sheet_key/pub?output=csv"
